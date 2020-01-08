bf54b981e565074a12b84a9a11b43182c805e042
Manuel Jahn <mjahn@geoblink.com>
2020-01-08 17:32:26 +0100

    Renamed interaction coefficient

--- potts_model_mc/config.py
+++ potts_model_mc/config.py
@@ -6,7 +6,7 @@ WIDTH = 12
 # Interesting temperature values are in the area of [1.5, 3.0]
 TEMPERATURE = 1.9
 INTERACTION = kronecker
-J_c = 1.
+J = 1.
 h = 0.
 
 VERBOSE = True
--- potts_model_mc/main.py
+++ potts_model_mc/main.py
@@ -1,6 +1,6 @@
 import tkinter
 
-from config import q, WIDTH, TEMPERATURE, INTERACTION, J_c, h, RANDOM_SEED
+from config import q, WIDTH, TEMPERATURE, INTERACTION, J, h, RANDOM_SEED
 from field import generate_field, calculate_energy_of_field, plot_field
 from gui import init_gui, draw_figure, update_energy_label
 from metropolis import update_metropolis
@@ -11,14 +11,14 @@ rg = RandomState(RANDOM_SEED)
 
 field = generate_field(states, WIDTH, rg, correlation=0.3)
 figure = plot_field(field, states)
-energy = calculate_energy_of_field(field, INTERACTION, interaction_coefficient=J_c, magnetization_coefficient=h)
+energy = calculate_energy_of_field(field, INTERACTION, interaction_coefficient=J, magnetization_coefficient=h)
 free_energy = 0
 mc_step = 0
 
 
 def update_step() -> None:
     global mc_step, field, free_energy, figure
-    field, free_energy = update_metropolis(field, states, free_energy, INTERACTION, interaction_coefficient=J_c,
+    field, free_energy = update_metropolis(field, states, free_energy, INTERACTION, interaction_coefficient=J,
                                            magnetization_coefficient=h, random_state=rg, temperature=TEMPERATURE)
     mc_step += 1
     figure = plot_field(field, states)
